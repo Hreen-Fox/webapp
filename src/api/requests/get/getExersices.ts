91eced56511c80c811c07bf7b5c0185fcd747745
FreakyPotion <wowcircle68@gmail.com>
2025-10-26 02:49:52 +0300

    fix get

--- src/api/requests/get/getExersices.ts
+++ src/api/requests/get/getExersices.ts
@@ -3,7 +3,7 @@ import {api} from '../../apiClient';
 // Получение списка упражнений
 export default async function getExercises(muscleGroupId: string, userId: string) {
     try {
-        const data = await api().get(`/myprograms/exercicesforselection/${muscleGroupId}/${userId}`);
+        const data = await api().get(`/myprograms/exercisesforselection/${muscleGroupId}/${userId}`);
 		return data;
 	} catch (error) {
 		console.error('Ошибка при загрузке дат тренировок: ', error);
